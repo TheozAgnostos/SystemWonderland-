44e07eefb817f50cab52a5f45149950deae70f5f
Theoz <kijoriz.exe@gmail.com>
2019-05-16 22:46:22 -0700

    First Load (Game Start) 05/16/19
    Initial Set-Up

---
+++ SW!/Assets/_PROJECTRPG/RPG_SCRIPTS/RPG_Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RPG_Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent nav;
+
+    void Start()  
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       nav.destination = target.position;
+    }
+}
